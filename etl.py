faa863bbb89c2b9dea48b02a47d960a502b33350
Omar Nasr Mohamed <omarnasr628@gmail.com>
2026-04-24 11:05:14 +0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -20,7 +20,7 @@ def create_spark_session():
     """
     spark = SparkSession \
         .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.6.0") \
         .getOrCreate()
     return spark
 
